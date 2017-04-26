024ae1382bfbf88df93160ef1ecd238d4f59357c
Nick Boughton <nicholasboughton@gmail.com>
2017-04-26 19:13:24 +0100

    d&D stats example added

---
+++ examples/d&d-stats/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/nboughton/go-dice"
+)
+
+func main() {
+	// Run once for each stat
+	for i := 0; i < 6; i++ {
+		n := []int{}
+		d, _ := dice.NewDice("1d6")
+
+		// roll 4 times
+		for j := 0; j < 4; j++ {
+			n = append(n, d.Roll())
+		}
+
+		sort.Ints(n)
+		fmt.Printf("%v = %d\n", n[1:], sum(n[1:]))
+	}
+}
+
+func sum(n []int) int {
+	t := 0
+	for _, v := range n {
+		t += v
+	}
+	return t
+}
